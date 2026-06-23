4db6575545a6f5ab15d551b7a3a22815c9ed99cf
khoitran200199-lab <khoitran200199@gmail.com>
2026-06-23 10:57:33 +0700

    chore: cleanup test and util scripts

--- test_bot.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-from telegram import Bot
-
-async def test():
-    bot = Bot("8526659236:AAHPuGIbaC3T_eNSg5FbUqKXmOpqwbr_bDA")
-    me = await bot.get_me()
-    print(me)
-
-asyncio.run(test())
--- test_worker2.mjs
+++
@@ -1,3 +0,0 @@
-import fs from 'fs';
-const code = fs.readFileSync('cloudflare_worker.js', 'utf8');
-console.log("length:", code.length);
